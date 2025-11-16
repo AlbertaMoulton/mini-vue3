6ae406b76ba816f503b27e5a4d128b045d8314e4
xkux8023 <chenyong8023@gmail.com>
2025-11-16 22:23:44 +0800

    feat: 完善reactive

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,3 +1,31 @@
-export function reactive() {
-    console.log("reactive")
+import { isObject } from "@vue/shared";
+import { ReactiveFlags } from "./constants";
+
+export function reactive(target: any) {
+  return createReactiveObject(target);
+}
+
+const reactiveMap = new WeakMap();
+const mutableHandlers = {
+  get(target, key, receiver) {
+    const res = Reflect.get(target, key, receiver);
+    return res;
+  },
+  set(target, key, value, receiver) {
+    const result = Reflect.set(target, key, value, receiver);
+    return result;
+  },
+};
+
+function createReactiveObject(target: any) {
+  console.log("reactive target:", target);
+  if (!isObject(target)) return target;
+  if (target[ReactiveFlags.IS_REACTIVE]) return target;
+
+  const existingProxy = reactiveMap.get(target);
+  if (existingProxy) return existingProxy;
+  
+  const proxy = new Proxy(target, mutableHandlers);
+  reactiveMap.set(target, proxy);
+  return proxy;
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,7 @@
 export function subtract(a: number, b: number): number {
     return a - b
 }
+
+export function isObject(value: any): boolean {
+    return value !== null && typeof value === 'object'
+}
